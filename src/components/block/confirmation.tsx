1d72a5aa64aef250e7cc0b17c1feec5cc11f77c9
Ashumendra-singh <ashu0609f@gmail.com>
2025-10-28 19:00:55 +0530

    asChild props added

--- src/components/block/confirmation.tsx
+++ src/components/block/confirmation.tsx
@@ -36,22 +36,24 @@ function Confirmation({onConfirmClick, onCancelClick, mainText, subText ,childre
   return (
     
     <Dialog >
-      <DialogTrigger>
+      <DialogTrigger asChild>
         {children}
       </DialogTrigger>
       <DialogContent showCloseButton={false} className=" bg-confirmation-color-bg border-confirmation-color-stroke w-[366px]">
         <DialogHeader className='flex-row justify-between'>
-          <DialogTitle className='confirmation-font-title'>Confirmation</DialogTitle>
+          <DialogTitle className='confirmation-font-title p-1'>Confirmation</DialogTitle>
           <DialogClose className='cursor-pointer'>
             <Icon name="Cross"></Icon>
           </DialogClose>
         </DialogHeader>
         
-        <DialogDescription className='confirmation-font-content text-confirmation-color-bodytext '>
-          {mainText}
-          <div className='mt-3 font-family-brandprimary text-color-text-neutral-tertiary font-family-brand-primary'>
+        <DialogDescription className='confirmation-font-content text-confirmation-color-bodytext flex flex-col gap-1'>
+          <span className='p-1'>
+            {mainText}
+          </span>
+          <span className='p-1 font-family-brandprimary text-color-text-neutral-tertiary font-family-brand-primary'>
             {subText}
-          </div>
+          </span>
         </DialogDescription>
         <DialogFooter className=' '>
           <Button variant="neutral" size="extraSmall" onClick={onCancelClick}>Cancle</Button>
